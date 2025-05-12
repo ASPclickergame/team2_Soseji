f5275b761357de78c9393a6b6e112c304f0f193d
Master Jiren <john38317@kw.ac.kr>
2025-05-12 21:01:07 +0900

    알바 시스템 추가
    알바 시스템 + 클릭시 돈 추가

--- Growing/Form1.Designer.cs
+++ Growing/Form1.Designer.cs
@@ -28,41 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.startBTN = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.firstJobTMR = new System.Windows.Forms.Timer(this.components);
+            this.secondJobTMR = new System.Windows.Forms.Timer(this.components);
+            this.thirdJobTMR = new System.Windows.Forms.Timer(this.components);
+            this.forthJobTMR = new System.Windows.Forms.Timer(this.components);
+            this.fifthJobTMR = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.moneyLBL = new System.Windows.Forms.Label();
+            this.fifthJobBTN = new System.Windows.Forms.Button();
+            this.forthJobBTN = new System.Windows.Forms.Button();
+            this.thirdJobBTN = new System.Windows.Forms.Button();
+            this.secondJobBTN = new System.Windows.Forms.Button();
+            this.firstJobBTN = new System.Windows.Forms.Button();
+            this.gameTMR = new System.Windows.Forms.Timer(this.components);
+            this.btncheckTMR = new System.Windows.Forms.Timer(this.components);
+            this.clickPNL = new System.Windows.Forms.Panel();
+            this.thirdJobTimerLBL = new System.Windows.Forms.Label();
+            this.fifthJobTimerLBL = new System.Windows.Forms.Label();
+            this.forthJobTimerLBL = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.secondJobTimerLBL = new System.Windows.Forms.Label();
+            this.firstJobTimerLBL = new System.Windows.Forms.Label();
+            this.updateCountdownTMR = new System.Windows.Forms.Timer(this.components);
+            this.clickPNL.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // startBTN
-            // 
-            this.startBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.startBTN.Location = new System.Drawing.Point(347, 461);
-            this.startBTN.Name = "startBTN";
-            this.startBTN.Size = new System.Drawing.Size(339, 125);
-            this.startBTN.TabIndex = 0;
-            this.startBTN.Text = "게임 시작";
-            this.startBTN.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(93, 274);
+            this.label1.Location = new System.Drawing.Point(13, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(866, 84);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Growing: 거지의 반란";
+            this.label1.Size = new System.Drawing.Size(73, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "모은돈";
+            // 
+            // moneyLBL
+            // 
+            this.moneyLBL.AutoSize = true;
+            this.moneyLBL.Location = new System.Drawing.Point(92, 14);
+            this.moneyLBL.Name = "moneyLBL";
+            this.moneyLBL.Size = new System.Drawing.Size(0, 21);
+            this.moneyLBL.TabIndex = 1;
+            // 
+            // fifthJobBTN
+            // 
+            this.fifthJobBTN.Location = new System.Drawing.Point(33, 676);
+            this.fifthJobBTN.Name = "fifthJobBTN";
+            this.fifthJobBTN.Size = new System.Drawing.Size(242, 50);
+            this.fifthJobBTN.TabIndex = 6;
+            this.fifthJobBTN.Text = "건물주";
+            this.fifthJobBTN.UseVisualStyleBackColor = true;
+            this.fifthJobBTN.Click += new System.EventHandler(this.fifthJobBTN_Click);
+            // 
+            // forthJobBTN
+            // 
+            this.forthJobBTN.Location = new System.Drawing.Point(33, 598);
+            this.forthJobBTN.Name = "forthJobBTN";
+            this.forthJobBTN.Size = new System.Drawing.Size(242, 50);
+            this.forthJobBTN.TabIndex = 7;
+            this.forthJobBTN.Text = "공무원";
+            this.forthJobBTN.UseVisualStyleBackColor = true;
+            this.forthJobBTN.Click += new System.EventHandler(this.forthJobBTN_Click);
+            // 
+            // thirdJobBTN
+            // 
+            this.thirdJobBTN.Location = new System.Drawing.Point(33, 520);
+            this.thirdJobBTN.Name = "thirdJobBTN";
+            this.thirdJobBTN.Size = new System.Drawing.Size(242, 50);
+            this.thirdJobBTN.TabIndex = 8;
+            this.thirdJobBTN.Text = "편의점 알바";
+            this.thirdJobBTN.UseVisualStyleBackColor = true;
+            this.thirdJobBTN.Click += new System.EventHandler(this.thirdJobBTN_Click);
+            // 
+            // secondJobBTN
+            // 
+            this.secondJobBTN.Location = new System.Drawing.Point(33, 441);
+            this.secondJobBTN.Name = "secondJobBTN";
+            this.secondJobBTN.Size = new System.Drawing.Size(242, 50);
+            this.secondJobBTN.TabIndex = 9;
+            this.secondJobBTN.Text = "전단지 알바";
+            this.secondJobBTN.UseVisualStyleBackColor = true;
+            this.secondJobBTN.Click += new System.EventHandler(this.secondJobBTN_Click);
+            // 
+            // firstJobBTN
+            // 
+            this.firstJobBTN.Location = new System.Drawing.Point(33, 373);
+            this.firstJobBTN.Name = "firstJobBTN";
+            this.firstJobBTN.Size = new System.Drawing.Size(242, 50);
+            this.firstJobBTN.TabIndex = 10;
+            this.firstJobBTN.Text = "노숙자";
+            this.firstJobBTN.UseVisualStyleBackColor = true;
+            this.firstJobBTN.Click += new System.EventHandler(this.firstJobBTN_Click);
+            // 
+            // clickPNL
+            // 
+            this.clickPNL.Controls.Add(this.thirdJobTimerLBL);
+            this.clickPNL.Controls.Add(this.fifthJobTimerLBL);
+            this.clickPNL.Controls.Add(this.forthJobTimerLBL);
+            this.clickPNL.Controls.Add(this.label4);
+            this.clickPNL.Controls.Add(this.secondJobTimerLBL);
+            this.clickPNL.Controls.Add(this.firstJobTimerLBL);
+            this.clickPNL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.clickPNL.Location = new System.Drawing.Point(0, 0);
+            this.clickPNL.Name = "clickPNL";
+            this.clickPNL.Size = new System.Drawing.Size(1043, 783);
+            this.clickPNL.TabIndex = 11;
+            this.clickPNL.Click += new System.EventHandler(this.clickPNL_Click);
+            // 
+            // thirdJobTimerLBL
+            // 
+            this.thirdJobTimerLBL.AutoSize = true;
+            this.thirdJobTimerLBL.Location = new System.Drawing.Point(281, 535);
+            this.thirdJobTimerLBL.Name = "thirdJobTimerLBL";
+            this.thirdJobTimerLBL.Size = new System.Drawing.Size(0, 21);
+            this.thirdJobTimerLBL.TabIndex = 5;
+            // 
+            // fifthJobTimerLBL
+            // 
+            this.fifthJobTimerLBL.AutoSize = true;
+            this.fifthJobTimerLBL.Location = new System.Drawing.Point(281, 691);
+            this.fifthJobTimerLBL.Name = "fifthJobTimerLBL";
+            this.fifthJobTimerLBL.Size = new System.Drawing.Size(0, 21);
+            this.fifthJobTimerLBL.TabIndex = 4;
+            // 
+            // forthJobTimerLBL
+            // 
+            this.forthJobTimerLBL.AutoSize = true;
+            this.forthJobTimerLBL.Location = new System.Drawing.Point(281, 613);
+            this.forthJobTimerLBL.Name = "forthJobTimerLBL";
+            this.forthJobTimerLBL.Size = new System.Drawing.Size(0, 21);
+            this.forthJobTimerLBL.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 21);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "label4";
+            // 
+            // secondJobTimerLBL
+            // 
+            this.secondJobTimerLBL.AutoSize = true;
+            this.secondJobTimerLBL.Location = new System.Drawing.Point(281, 456);
+            this.secondJobTimerLBL.Name = "secondJobTimerLBL";
+            this.secondJobTimerLBL.Size = new System.Drawing.Size(0, 21);
+            this.secondJobTimerLBL.TabIndex = 1;
+            // 
+            // firstJobTimerLBL
+            // 
+            this.firstJobTimerLBL.AutoSize = true;
+            this.firstJobTimerLBL.Location = new System.Drawing.Point(281, 388);
+            this.firstJobTimerLBL.Name = "firstJobTimerLBL";
+            this.firstJobTimerLBL.Size = new System.Drawing.Size(0, 21);
+            this.firstJobTimerLBL.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1043, 783);
+            this.Controls.Add(this.firstJobBTN);
+            this.Controls.Add(this.secondJobBTN);
+            this.Controls.Add(this.thirdJobBTN);
+            this.Controls.Add(this.forthJobBTN);
+            this.Controls.Add(this.fifthJobBTN);
+            this.Controls.Add(this.moneyLBL);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.startBTN);
+            this.Controls.Add(this.clickPNL);
             this.Name = "Form1";
             this.Text = "Growing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.clickPNL.ResumeLayout(false);
+            this.clickPNL.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,8 +210,28 @@
 
         #endregion
 
-        private System.Windows.Forms.Button startBTN;
+        private System.Windows.Forms.Timer firstJobTMR;
+        private System.Windows.Forms.Timer secondJobTMR;
+        private System.Windows.Forms.Timer thirdJobTMR;
+        private System.Windows.Forms.Timer forthJobTMR;
+        private System.Windows.Forms.Timer fifthJobTMR;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label moneyLBL;
+        private System.Windows.Forms.Button fifthJobBTN;
+        private System.Windows.Forms.Button forthJobBTN;
+        private System.Windows.Forms.Button thirdJobBTN;
+        private System.Windows.Forms.Button secondJobBTN;
+        private System.Windows.Forms.Button firstJobBTN;
+        private System.Windows.Forms.Timer gameTMR;
+        private System.Windows.Forms.Timer btncheckTMR;
+        private System.Windows.Forms.Panel clickPNL;
+        private System.Windows.Forms.Label thirdJobTimerLBL;
+        private System.Windows.Forms.Label fifthJobTimerLBL;
+        private System.Windows.Forms.Label forthJobTimerLBL;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label secondJobTimerLBL;
+        private System.Windows.Forms.Label firstJobTimerLBL;
+        private System.Windows.Forms.Timer updateCountdownTMR;
     }
 }
 
--- Growing/Form1.cs
+++ Growing/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlTypes;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,213 @@ namespace Growing
 {
     public partial class Form1 : Form
     {
+        private int money = 0;
+        private List<Button> hireButtons;
+        private Dictionary<Timer, Worker> timerToWorker;
+        private Dictionary<Timer, Label> timerToLabel;
+        private Dictionary<Timer, int> timerRemainingTime;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Worker 리스트
+        private List<Worker> workers = new List<Worker>
+        {
+            new Worker("노숙자", 3000, 100, 3000),
+            new Worker("전단지 알바", 10000, 500, 10000),
+            new Worker("편의점 알바", 300000, 3000, 30000),
+            new Worker("공무원", 600000, 10000, 60000),
+            new Worker("건물주", 1800000, 50000, 180000)
+        };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+
+            // 버튼 순서 일치
+            hireButtons = new List<Button> { firstJobBTN, secondJobBTN, thirdJobBTN, forthJobBTN, fifthJobBTN };
+
+            // 버튼 상태 0.1초마다 체크
+            btncheckTMR.Interval = 100;
+            btncheckTMR.Tick += (s, eArgs) => UpdateButtonStates();
+            btncheckTMR.Start();
+
+            // 1초마다 10원 자동 수익
+            gameTMR.Interval = 100;
+            gameTMR.Tick += (s, eArgs) =>
+            {
+                money += 1;
+                UpdateMoneyLabel();
+            };
+            gameTMR.Start();
+
+            // 각 알바 타이머 -> 알바 매핑
+            timerToWorker = new Dictionary<Timer, Worker>
+            {
+                { firstJobTMR, workers[0] },
+                { secondJobTMR, workers[1] },
+                { thirdJobTMR, workers[2] },
+                { forthJobTMR, workers[3] },
+                { fifthJobTMR, workers[4] }
+            };
+
+            // 타이머마다 Tick 이벤트 설정
+            foreach (var pair in timerToWorker)
+            {
+                pair.Key.Interval = pair.Value.Interval;
+                Worker w = pair.Value;
+            }
+
+            // 타이머 -> 남은 시간 표시할 라벨
+            timerToLabel = new Dictionary<Timer, Label>
+            {
+                { firstJobTMR, firstJobTimerLBL },
+                { secondJobTMR, secondJobTimerLBL },
+                { thirdJobTMR, thirdJobTimerLBL },
+                { forthJobTMR, forthJobTimerLBL },
+                { fifthJobTMR, fifthJobTimerLBL }
+            };
+
+            // 타이머 -> 남은 시간 (ms)
+            timerRemainingTime = new Dictionary<Timer, int>();
+            foreach (var pair in timerToWorker)
+            {
+                timerRemainingTime[pair.Key] = pair.Value.Interval;
+            }
+
+            updateCountdownTMR.Interval = 100; // 0.1초 간격으로 정밀하게 표시
+            updateCountdownTMR.Tick += UpdateRemainingTimes;
+            updateCountdownTMR.Start();
+
+            UpdateButtonStates();
+        }
+
+        private void HireWorker(Worker worker, Timer tmr)
+        {
+            if (money >= worker.Cost && !worker.IsHired)
+            {
+                money -= worker.Cost;
+                worker.IsHired = true;
+                tmr.Start();
+                UpdateMoneyLabel();
+            }
+        }
+
+        private void UpdateMoneyLabel()
+        {
+            moneyLBL.Text = $"{money}원";
+        }
+
+        private void UpdateButtonStates()
+        {
+            for (int i = 0; i < workers.Count; i++)
+            {
+                Worker w = workers[i];
+                Button btn = hireButtons[i];
+
+                if (w.IsHired)
+                {
+                    btn.Enabled = false;
+                    btn.Text = $"{w.Name}: {w.Interval / 1000}초당 {w.Income:N0}원";
+                    btn.BackColor = Color.LightGreen;
+                }
+                else if (money >= w.Cost)
+                {
+                    btn.Enabled = true;
+                    btn.Text = $"{w.Name} : {w.Cost:N0}원";
+                    btn.BackColor = SystemColors.Control; // 기본 색으로 복원
+                }
+                else
+                {
+                    btn.Enabled = false;
+                    btn.Text = $"{w.Name} : {w.Cost:N0}원";
+                    btn.BackColor = Color.LightCoral; // 빨간색 느낌의 비활성 색
+                }
+            }
+        }
+
+
+        private void UpdateRemainingTimes(object sender, EventArgs e)
+        {
+            foreach (var pair in timerToWorker)
+            {
+                Timer tmr = pair.Key;
+                Worker worker = pair.Value;
+
+                if (!worker.IsHired)
+                {
+                    timerToLabel[tmr].Text = "";
+                    continue;
+                }
+
+                timerRemainingTime[tmr] -= updateCountdownTMR.Interval;
+
+                if (timerRemainingTime[tmr] <= 0)
+                {
+                    // 수익 지급
+                    money += worker.Income;
+                    UpdateMoneyLabel();
+
+                    // 타이머 초기화
+                    timerRemainingTime[tmr] = worker.Interval;
+                }
+
+                double secondsLeft = timerRemainingTime[tmr] / 1000.0;
+                timerToLabel[tmr].Text = $"남은 시간: {secondsLeft:0.0}초";
+            }
+        }
+
+
+
+        private void firstJobBTN_Click(object sender, EventArgs e)
+        {
+            HireWorker(workers[0], firstJobTMR);
+        }
+
+        private void secondJobBTN_Click(object sender, EventArgs e)
+        {
+            HireWorker(workers[1], secondJobTMR);
+        }
+
+        private void thirdJobBTN_Click(object sender, EventArgs e)
+        {
+            HireWorker(workers[2], thirdJobTMR);
+        }
+
+        private void forthJobBTN_Click(object sender, EventArgs e)
+        {
+            HireWorker(workers[3], forthJobTMR);
+        }
+
+        private void fifthJobBTN_Click(object sender, EventArgs e)
+        {
+            HireWorker(workers[4], fifthJobTMR);
+        }
+
+        private void clickPNL_Click(object sender, EventArgs e)
+        {
+            money += 100;
+            UpdateMoneyLabel();
+        }
+
+    }
+
+    public class Worker
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public int Income { get; set; }
+        public int Interval { get; set; } // ms
+        public bool IsHired { get; set; } = false;
+
+        public Worker(string name, int cost, int income, int interval)
+        {
+            Name = name;
+            Cost = cost;
+            Income = income;
+            Interval = interval;
         }
     }
 }
