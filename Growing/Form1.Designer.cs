a5b0c28208099b512184cdae466d55ca479f4a7e
Master Jiren <john38317@kw.ac.kr>
2025-05-12 18:49:56 +0900

    5/12 박요한

---
+++ Growing/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Growing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startBTN
+            // 
+            this.startBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startBTN.Location = new System.Drawing.Point(347, 461);
+            this.startBTN.Name = "startBTN";
+            this.startBTN.Size = new System.Drawing.Size(339, 125);
+            this.startBTN.TabIndex = 0;
+            this.startBTN.Text = "게임 시작";
+            this.startBTN.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(93, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(866, 84);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Growing: 거지의 반란";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 783);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startBTN);
+            this.Name = "Form1";
+            this.Text = "Growing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBTN;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
